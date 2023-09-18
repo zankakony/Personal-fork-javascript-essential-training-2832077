a5032737a639b08698d077efaf633f53d380509f
Sameh Zankalony <zankalony@gmail.com>
2023-09-18 19:50:12 +0300

    update

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,6 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+console.log(document.getElementById("pack02"));
+console.log(document.querySelector(".sitefooter"));
+console.log(document.querySelector(".backpack__image"));
